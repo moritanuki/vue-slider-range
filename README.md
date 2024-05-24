9b02c374b8c22acbf69b53c0860d451cf91d65e2
moritanuki <ilovfam12@gmail.com>
2024-05-24 16:08:15 +0900

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# スライダーボタンの実験
+
+## 使用技術
+- Vue 3
+- TypeScript
+- Vite
+- vueform/slider
---
+++ src/components/VueSliderRange.vue
@@ -0,0 +1,42 @@
+<template class="wrapper">
+  <div class="slider">
+    <p class="title">ぬるぬる動く自作step</p>
+    <Slider v-model="value" :min="0" :max="100" :lazy="false" @change="changeVolume()" />
+  </div>
+  <div class="slider">
+    <p class="title">カクカク動くライブラリのstep</p>
+    <Slider v-model="stepValue" :min="0" :max="100" :lazy="false" :step="50" />
+  </div>
+</template>
+<script>
+  import Slider from '@vueform/slider'
+
+  export default {
+    components: {
+      Slider,
+    },
+    data() {
+      return {
+        value: 0,
+        stepValue: 0,
+      }
+    },
+    methods: {
+      changeVolume() {
+        // 50ずつのステップ
+        this.value = Math.round(this.value / 50) * 50;
+      }
+    }
+  }
+</script>
+<style src="@vueform/slider/themes/default.css"></style>
+<style scoped>
+.title {
+  text-align: center;
+}
+
+.slider {
+  width: 500px;
+  margin: 50px;
+}
+</style>

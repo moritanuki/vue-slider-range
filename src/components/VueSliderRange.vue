d462b2a473905ddc81e09b8f3d1d6e97ddf237fe
moritanuki <ilovfam12@gmail.com>
2024-05-24 16:57:18 +0900

    コンポーネント化
    * vueform/slider

--- src/components/VueSliderRange.vue
+++ src/components/VueSliderRange.vue
@@ -1,37 +1,17 @@
 <template class="wrapper">
-  <div class="slider">
-    <p class="title">ぬるぬる動く自作step</p>
-    <Slider v-model="value" :min="0" :max="100" :lazy="false" @change="changeVolume()" />
-  </div>
-  <div class="slider">
-    <p class="title">カクカク動くライブラリのstep</p>
-    <Slider v-model="stepValue" :min="0" :max="100" :lazy="false" :step="50" />
-  </div>
+  <VueformSlider />
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Slider from '@vueform/slider';
+import VueformSlider from './VueformSlider.vue';
 
 export default defineComponent({
   components: {
-    Slider,
-  },
-  data() {
-    return {
-      value: 0,
-      stepValue: 0,
-    };
-  },
-  methods: {
-    changeVolume() {
-      // 50ずつのステップ
-      this.value = Math.round(this.value / 50) * 50;
-    },
+    VueformSlider,
   },
 });
 </script>
-<style src="@vueform/slider/themes/default.css"></style>
-<style scoped>
+<style>
 .title {
   text-align: center;
   margin: 40px;

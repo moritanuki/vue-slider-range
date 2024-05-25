5ed71c98eb773407240320017dcce6f3ed439ce6
moritanuki <ilovfam12@gmail.com>
2024-05-25 13:29:41 +0900

    よりぬるぬるに
    * ぬるぬる強化
    * tooltip表示も改善

--- src/components/VueformSlider.vue
+++ src/components/VueformSlider.vue
@@ -1,7 +1,10 @@
 <template class="wrapper">
   <div class="slider">
     <p class="title">ぬるぬる動く自作step</p>
-    <Slider v-model="value" :min="0" :max="100" :lazy="false" @change="changeVolume()" />
+    <!-- NOTE: 本来のinput要素であれば、step値にanyを割り当てられる（小数点何桁でも受け入れ可能）が、
+    vueform/sliderは数値のみ受け付けているためTypeScriptで弾かれてしまう。-1で同様の効果が狙える。 -->
+    <Slider v-model="value" :min="0" :max="100" :lazy="false" :step="-1" :format="changeVolume"
+      @change="changeVolume()" />
   </div>
   <div class="slider">
     <p class="title">カクカク動くライブラリのstep</p>
@@ -26,6 +29,11 @@ export default defineComponent({
     changeVolume() {
       // 50ずつのステップ
       this.value = Math.round(this.value / 50) * 50;
+
+      // NOTE: tooltipに表示させる値
+      // tooltipの表記はvalue値に依存するため、
+      // changeイベントでvalue値を書き換えるタイミングで同期するようにしている
+      return this.value
     },
   },
 });
